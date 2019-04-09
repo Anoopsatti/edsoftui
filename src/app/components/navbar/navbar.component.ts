6ce732b06f269a3141793e000a55090e2b105392
anoop <svranoop90@gmail.com>
2019-04-09 16:27:46 +0530

    added 404page

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -14,7 +14,7 @@ export class NavbarComponent implements OnInit {
   constructor(
     private router: Router,
     private ngFlashMessageService: NgFlashMessageService,
-    private authService: AuthService
+    public authService: AuthService
   ) { }
 
   ngOnInit(): void {
